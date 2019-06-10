4a76c5b197e2f95439c72a9511330f6f8b23a1cb
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-06-10 19:38:51 +0200

    Long Method, extract switch statement method, Extract Method

--- src/Customer.h
+++ src/Customer.h
@@ -17,6 +17,8 @@ public:
 private:
     std::string _name;
     std::vector< Rental > _rentals;
+
+    double amount(double thisAmount, const Rental &each) const;
 };
 
 inline Customer::Customer() {}
