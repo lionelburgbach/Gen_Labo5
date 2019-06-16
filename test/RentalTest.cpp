5df5dd30031b78bad6e03450df1270b53aa6f654
ncdm-stldr <ncdm.stldr@gmail.com>
2019-06-16 04:18:42 +0200

    Move statement to funtion: Customer now call method frequentRenterPoints on the Rental. Bad smell: Shotgun Surgery

--- test/RentalTest.cpp
+++ test/RentalTest.cpp
@@ -13,5 +13,21 @@ TEST(rentalTest, amount)
     double thisAmount = 0;
     thisAmount += each.amount();
 
+
+
     EXPECT_EQ(thisAmount, 14);
 }
+
+TEST(rentalTest2, getRenterPoints)
+{
+    Rental r1 = Rental( Movie("Interstellar"), 10);
+    Rental r2 = Rental( Movie("Interstellar2"), 1);
+    Rental r3 = Rental( Movie("Godzilla 57", Movie::NEW_RELEASE), 1);
+    Rental r4 = Rental( Movie("Godzilla 57", Movie::NEW_RELEASE), 18);
+
+    EXPECT_EQ(r1.frequentRenterPoints(), 1);
+    EXPECT_EQ(r2.frequentRenterPoints(), 1);
+    EXPECT_EQ(r3.frequentRenterPoints(), 1);
+    EXPECT_EQ(r4.frequentRenterPoints(), 2);
+}
+
